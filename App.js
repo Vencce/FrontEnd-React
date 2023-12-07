374eb193d0dcc9eb9e09637af61d58b329abe0df
vencce <vitorferreira20005@gmial.com>
2023-12-07 10:39:16 -0300

    Login funcional

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
-import React, {useState, useEffect} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Home, Login, Cadastro} from './views/Routes/index'
-import AreaRestrita from './views/AreaRestrita/AreaRestrita';
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Home, Login, Cadastro } from "./views/Routes/index";
+import AreaRestrita from "./views/AreaRestrita/AreaRestrita";
+import { RecoilRoot } from 'recoil';
 
 export default function App() {
-
   const Stack = createNativeStackNavigator();
 
   return (
+    <RecoilRoot>
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen 
-          name='AreaRestrita' 
+        <Stack.Screen
+          name="Login"
+          options={{ headerShown: false }}
+          component={Login}
+        />
+        <Stack.Screen
+          name="AreaRestrita"
           component={AreaRestrita}
           options={{
-            headerShown:false,
-            title: 'AreaRestrita'
+            headerShown: false,
+            title: "AreaRestrita",
           }}
         />
-        <Stack.Screen name="Login" options={{headerShown:false}} component={Login} />
-        <Stack.Screen name="Cadastro" options={{headerShown:false}} component={Cadastro} />
+        <Stack.Screen
+          name="Cadastro"
+          options={{ headerShown: false }}
+          component={Cadastro}
+          />
       </Stack.Navigator>
-    </NavigationContainer>  
+    </NavigationContainer>
+    </RecoilRoot>
   );
 }
--- views/Login_Cadastro/Login.js
+++ views/Login_Cadastro/Login.js
@@ -10,13 +10,46 @@ import {
 } from "react-native";
 import { css } from "../../assets/css/Css";
 import Icon from "react-native-vector-icons/FontAwesome";
+import * as SecureStore from 'expo-secure-store';
+import { useSetRecoilState } from 'recoil'  
+
+import LoginApi from '../../seeders/services/login';
+import { userState } from '../recoil/atoms/auth';
 
 export default function Login({ navigation }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    navigation.navigate("AreaRestrita");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const setUser = useSetRecoilState(userState);
+
+  const login = async () => {
+    try {
+      const data = await LoginApi.login(username, password);
+      setUser({
+        loggedIn: true,
+        access: data.access,
+        refresh: data.refresh,
+      });
+      setUsername('');
+      setPassword('');
+      setErrorMsg(null);
+
+      if (Platform.OS === 'android' || Platform.OS === 'ios') {
+        await SecureStore.setItemAsync('access', data.access);
+        await SecureStore.setItemAsync('refresh', data.refresh);
+      }
+  
+      navigation.navigate('AreaRestrita');
+    } catch (error) {
+      setUser({ loggedIn: false, access: null, refresh: null });
+      setErrorMsg('Usuário ou senha inválidos!');
+  
+      if (Platform.OS === 'android' || Platform.OS === 'ios') {
+        await SecureStore.deleteItemAsync('access');
+      }
+    }
   };
 
   return (
@@ -29,27 +62,27 @@ export default function Login({ navigation }) {
         <View style={css.textinput}>
           <Icon name="user-circle" size={20} color="grey" />
           <TextInput
+            label="Usuário"
             style={css.placeholderLogin}
             placeholder="Usuário:"
             value={username}
-            onChangeText={(username) => setUsername(username)}
+            onChangeText={setUsername}
           />
         </View>
         <View style={css.textinput}>
           <Icon name="lock" size={20} color="grey" />
           <TextInput
+            label="Password"
+            type="password"
             style={css.placeholderLogin}
             placeholder="Senha:"
             secureTextEntry={true}
             value={password}
-            onChangeText={(password) => setPassword(password)}
+            onChangeText={setPassword}
           />
-          <TouchableOpacity style={css.iconeye}>
-            <Icon name="eye" size={20} color="grey" />
-          </TouchableOpacity>
         </View>
 
-        <TouchableOpacity onPress={handleLogin}>
+        <TouchableOpacity onPress={() => login()}>
           <Text style={css.textinputbtn}>Entrar</Text>
         </TouchableOpacity>
       </View>
@@ -58,6 +91,7 @@ export default function Login({ navigation }) {
         <Text style={css.cadastro}>Não tem conta? cadastre-se!</Text>
       </TouchableOpacity>
 
+      <Text>{errorMsg}</Text>
       <View style={css.nadabranco}></View>
     </KeyboardAvoidingView>
   );
